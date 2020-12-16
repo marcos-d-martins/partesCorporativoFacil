f414b4806a96f64b9894831504b5f77356a43b37
Marcos D <madthrasher13@gmail.com>
2020-12-16 20:18:36 -0300

    Create empresas.php

---
+++ empresas.php
@@ -0,0 +1,9 @@
+<table>
+   <tr>
+        <td>
+            <img src="<?= PAGINA_INICIAL;?>tim.php?src=https://localhost/CorporativoFacil/<?= $logotipo?>" title="<?= $nome_empresa;?>">
+        </td>
+        <td class="valores"><?= $nome_empresa;?></td>
+        <td class="valores"><?= $endereco;?></td>
+    </tr>
+</table>
